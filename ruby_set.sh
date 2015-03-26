32d5534cc1dac64dee5b7bca85e15778edf901a0
Abhishek Modi <abhishek.modi95@gmail.com>
2015-03-26 14:33:09 -0500

    ruby setup. It's only temporary

---
+++ ruby_set.sh
@@ -0,0 +1,20 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install ruby
+sudo apt-get install ruby-dev
+sudo apt-get install libxslt1-dev
+sudo apt-get install libxml2-dev
+sudo apt-get install zlib1g-dev
+
+sudo gem install nokogiri
+sudo gem install rails
+
+sudo apt-get install python
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+sudo pip install twilio
+sudo pip install ipython
+
+sudo apt-get install subversion
+sudo apt-get install git
