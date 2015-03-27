851b086b77ca98e9356dc17a69c71a189caa239d
Abhishek Modi <abhishek.modi95@gmail.com>
2015-03-27 16:09:36 -0500

    cleaned wget + sql

--- ruby_set.sh
+++ ruby_set.sh
@@ -6,15 +6,22 @@ sudo apt-get install ruby-dev
 sudo apt-get install libxslt1-dev
 sudo apt-get install libxml2-dev
 sudo apt-get install zlib1g-dev
+sudo apt-get install sqlite3
+sudo apt-get install libsqlite3-dev
 
+sudo gem install rvm
 sudo gem install nokogiri
+sudo gem install sqlite3
 sudo gem install rails
+sudo gem install railties
+sudo gem install executable-hooks
+sudo gem install gem-wrappers
+sudo gem install rubygems-bundler
 
 sudo apt-get install python
-wget https://bootstrap.pypa.io/get-pip.py
-sudo python get-pip.py
-sudo pip install twilio
+sudo apt-get install python-pip
 sudo pip install ipython
+sudo pip install twilio
 
 sudo apt-get install subversion
 sudo apt-get install git
